93fe561e223df191d5049be0acaf8ee64a171c41
이준명 <liam@hanatour.com>
2024-05-20 08:34:10 +0900

    2024.05.20

--- js/script.js
+++ js/script.js
@@ -8,21 +8,12 @@ function do_process() {
   showById("div_answer");
   showById("ul_submit_btn");
 
-
-  $('#input_answer').focus();
+  $("#input_answer").focus();
   // 마지막
   // oy_go_next();
 }
 
 function checkAnswer() {
-  if (question_index === init_content["data"].length - 1) {
-    
-    console.log("모든 정답 완료");
-    // 페이지 이동
-    oy_go_next();
-    return;
-  }
-
   let question = init_content["data"][question_index]["question"];
   let answer = init_content["data"][question_index]["answer"];
   let input_answer = $("#input_answer").val();
@@ -35,11 +26,18 @@ function checkAnswer() {
     $("#input_answer").val("");
     question_index += 1;
 
-    // 문제 세팅
-    showQuestion();
+    if (question_index === init_content["data"].length) {
+      console.log("모든 정답 완료");
+      // 페이지 이동
+      oy_go_next();
+      return;
+    } else {
+      // 문제 세팅
+      showQuestion();
+    }
   } else {
     // 답변 틀렸을 경우
-    alert('다영이가 아니면 예쁜사진 봤으니 나가라!');
+    alert("다영이가 아니면 예쁜사진 봤으니 나가라!");
     $("#input_answer").val("");
   }
 }
@@ -54,7 +52,7 @@ function showById(_id) {
 
 function showQuestion() {
   $("#p_question").text(init_content["data"][question_index]["question"]);
-  $('#input_answer').focus();
+  $("#input_answer").focus();
 }
 
 function oy_show_note() {
@@ -68,7 +66,7 @@ function oy_hide_note() {
 function oy_go_next() {
   //$("#div_oy_yes").show();
 
-  alert('다영이가 맞구나!! 여보!! ㅠㅠ');
+  alert("다영이가 맞구나!! 여보!! ㅠㅠ");
 
   setTimeout(function () {
     $("#div_onlyyou").fadeOut();
