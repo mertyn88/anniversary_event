483e7d18502a6a4bf18f01f0f5ce6887620f3d55
이준명 <liam@hanatour.com>
2024-08-27 13:14:11 +0900

    2024.08.27

---
+++ diary/js/images.js
@@ -0,0 +1,50 @@
+const galleryData = [
+	{
+		caption: "2024년 8월 14일.<br>소중한 첫만남에서",
+		photo: "images/2024_08_14.png"
+	},
+	{
+		caption: "2024년 8월 15일.<br>기다렸던 첫번째 면회에서",
+		photo: "images/2024_08_15.png"
+	},
+	{
+		caption: "2024년 8월 16일.<br>두번째 면회 신생아실에서",
+		photo: "images/2024_08_16.png"
+	},
+	{
+		caption: "2024년 8월 17일.<br>첫 눈맞춤 캥거루케어실에서",
+		photo: "images/2024_08_17.png"
+	},
+	{
+		caption: "2024년 8월 18일.<br>마지막 면회에서",
+		photo: "images/2024_08_18.png"
+	},
+	{
+		caption: "Pangolin",
+		photo: "images/2024_08_19.png"
+	},
+	{
+		caption: "Queen Bee",
+		photo: "images/2024_08_20.png"
+	},
+	{
+		caption: "Rhinoceros",
+		photo: "images/2024_08_21.png"
+	},
+	{
+		caption: "Snake",
+		photo: "images/2024_08_22.png"
+	},
+	{
+		caption: "Snake",
+		photo: "images/2024_08_23.png"
+	},
+	{
+		caption: "Snake",
+		photo: "images/2024_08_24.png"
+	},
+	{
+		caption: "Snake",
+		photo: "images/2024_08_25.png"
+	}
+];
