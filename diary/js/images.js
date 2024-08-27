65b9ea748cc1a055daa4e895524da293b8755b3a
이준명 <liam@hanatour.com>
2024-08-27 16:35:47 +0900

    2024.08.27

--- diary/js/images.js
+++ diary/js/images.js
@@ -1,46 +1,46 @@
 const galleryData = [
 	{
-		caption: "2024년 8월 14일.<br>소중한 첫만남에서",
+		caption: "2024년 8월 14일.<br>소중한 첫만남",
 		photo: "images/2024_08_14.png"
 	},
 	{
-		caption: "2024년 8월 15일.<br>기다렸던 첫번째 면회에서",
+		caption: "2024년 8월 15일.<br>우리의 첫번째 면회",
 		photo: "images/2024_08_15.png"
 	},
 	{
-		caption: "2024년 8월 16일.<br>두번째 면회 신생아실에서",
+		caption: "2024년 8월 16일.<br>신생아실에서의 두번째 면회",
 		photo: "images/2024_08_16.png"
 	},
 	{
-		caption: "2024년 8월 17일.<br>첫 눈맞춤 캥거루케어실에서",
+		caption: "2024년 8월 17일.<br>아빠와의 첫 눈맞춤. 캥거루케어실",
 		photo: "images/2024_08_17.png"
 	},
 	{
-		caption: "2024년 8월 18일.<br>마지막 면회에서",
+		caption: "2024년 8월 18일.<br>아빠의 마지막 면회",
 		photo: "images/2024_08_18.png"
 	},
 	{
-		caption: "Pangolin<br>text",
+		caption: "2024년 8월 19일.<br>수유실 해인이 맘마타임",
 		photo: "images/2024_08_19.png"
 	},
 	{
-		caption: "Queen Bee<br>text",
+		caption: "2024년 8월 20일.<br>해인이 나라의 일꾼 등록",
 		photo: "images/2024_08_20.png"
 	},
 	{
-		caption: "Rhinoceros<br>text",
+		caption: "2024년 8월 21일.<br>초보엄마의 모자동실",
 		photo: "images/2024_08_21.png"
 	},
 	{
-		caption: "Snake<br>text",
+		caption: "2024년 8월 22일.<br>바라만봐도 행복해",
 		photo: "images/2024_08_22.png"
 	},
 	{
-		caption: "Snake<br>text",
+		caption: "2024년 8월 23일.<br>엄마의 도전 (기저귀 갈아보기)",
 		photo: "images/2024_08_23.png"
 	},
 	{
-		caption: "Snake<br>text",
+		caption: "2024년 8월 24일.<br>키대결 (해인이 vs 고양이인형)",
 		photo: "images/2024_08_24.png"
 	},
 	{
