c7246a66785ab64054ea730ece274e49704fd6de
Liam <mertyn88@gmail.com>
2024-08-28 20:49:27 +0900

    no message

--- diary/js/images.js
+++ diary/js/images.js
@@ -1,50 +1,62 @@
 const galleryData = [
 	{
-		caption: "2024년 8월 14일.<br>소중한 첫만남",
+		date: "2024년 8월 14일.",
+		caption: "해인이와의 소중한 첫만남",
 		photo: "images/2024_08_14.png"
 	},
 	{
-		caption: "2024년 8월 15일.<br>우리의 첫번째 면회",
+		date: "2024년 8월 15일.",
+		caption: "신생아실에서의 첫번째 면회",
 		photo: "images/2024_08_15.png"
 	},
 	{
-		caption: "2024년 8월 16일.<br>신생아실에서의 두번째 면회",
+		date: "2024년 8월 16일.",
+		caption: "신생아실에서의 두번째 면회",
 		photo: "images/2024_08_16.png"
 	},
 	{
-		caption: "2024년 8월 17일.<br>아빠와의 첫 눈맞춤. 캥거루케어실",
+		date: "2024년 8월 17일.",
+		caption: "아빠와의 첫 눈맞춤. 캥거루케어실",
 		photo: "images/2024_08_17.png"
 	},
 	{
-		caption: "2024년 8월 18일.<br>아빠의 마지막 면회",
+		date: "2024년 8월 18일.",
+		caption: "아빠의 마지막 면회",
 		photo: "images/2024_08_18.png"
 	},
 	{
-		caption: "2024년 8월 19일.<br>수유실 해인이 맘마타임",
+		date: "2024년 8월 19일.",
+		caption: "수유실 해인이 맘마타임",
 		photo: "images/2024_08_19.png"
 	},
 	{
-		caption: "2024년 8월 20일.<br>해인이 나라의 일꾼 등록",
+		date: "2024년 8월 20일.",
+		caption: "해인이 나라의 일꾼 등록",
 		photo: "images/2024_08_20.png"
 	},
 	{
-		caption: "2024년 8월 21일.<br>초보엄마의 모자동실",
+		date: "2024년 8월 21일.",
+		caption: "초보엄마의 모자동실",
 		photo: "images/2024_08_21.png"
 	},
 	{
-		caption: "2024년 8월 22일.<br>바라만봐도 행복해",
+		date: "2024년 8월 22일.",
+		caption: "바라만봐도 행복해",
 		photo: "images/2024_08_22.png"
 	},
 	{
-		caption: "2024년 8월 23일.<br>엄마의 도전 (기저귀 갈아보기)",
+		date: "2024년 8월 23일.",
+		caption: "엄마의 도전 (기저귀 갈아보기)",
 		photo: "images/2024_08_23.png"
 	},
 	{
-		caption: "2024년 8월 24일.<br>키대결 (해인이 vs 고양이인형)",
+		date: "2024년 8월 24일.",
+		caption: "키 대결 (해인이 vs 고양이인형)",
 		photo: "images/2024_08_24.png"
 	},
 	{
-		caption: "2024년 8월 25일.<br>여러 표정을 짓는 해인이",
+		date: "2024년 8월 25일.",
+		caption: "여러 표정을 짓는 해인이",
 		photo: "images/2024_08_25.png"
 	}
 ];
